ec504ae08f19139ed2b15a3e46ba0a3c932561c3
Bernardo Mendes <bernardomennndes@outlook.com>
2023-02-01 22:56:23 -0300

    [ADD] Generate certification function

--- src/functions/generateCertification.ts
+++ src/functions/generateCertification.ts
@@ -1,26 +1,35 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
+import { join } from "path";
+import { compile } from "handlebars";
+import { readFileSync } from "fs";
+import dayjs from "dayjs";
+import chromium from "chrome-aws-lambda";
+import { S3 } from "aws-sdk";
 
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
 interface ICreateCertification {
   id: string;
   name: string;
   grade: string;
 }
 
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+};
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertification;
-
-  await document
-    .put({
-      TableName: "users_certifications",
-      Item: {
-        id,
-        name,
-        grade,
-        created_at: new Date().getTime(),
-      },
-    })
-    .promise();
 
   const response = await document
     .query({
@@ -32,8 +41,73 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     })
     .promise();
 
+  const userAlreadyExists = response.Items[0];
+
+  if (!userAlreadyExists) {
+    await document
+      .put({
+        TableName: "users_certifications",
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
+  }
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    id,
+    name,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal,
+  };
+
+  const content = await compileTemplate(data);
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    userDataDir: "/dev/null",
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? "./certification.pdf" : null,
+  });
+
+  browser.close();
+
+  const s3 = new S3();
+
+  await s3
+    .putObject({
+      Bucket: "node-js-certification-ignite",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: pdf,
+      ContentType: "application/pdf",
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0]),
+    body: JSON.stringify({
+      message: "Certification successfully created!",
+      url: `https://node-js-certification-ignite.s3.amazonaws.com/${id}.pdf`,
+    }),
   };
 };
