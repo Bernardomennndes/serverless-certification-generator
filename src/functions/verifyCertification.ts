ba1663978a9730e0984de5326b1be9999e60d65e
Bernardo Mendes <bernardomennndes@outlook.com>
2023-02-01 22:56:57 -0300

    [ADD] verify certification function

---
+++ src/functions/verifyCertification.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+
+interface IUserCertification {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { id } = event.pathParameters;
+
+  const response = await document
+    .query({
+      TableName: "users_certifications",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": id,
+      },
+    })
+    .promise();
+
+  const userCertification = response.Items[0] as IUserCertification;
+
+  if (userCertification) {
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: "Valid certification.",
+        name: userCertification.name,
+        url: `https://node-js-certification-ignite.s3.amazonaws.com/${userCertification.id}.pdf`,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Invalid certification.",
+    }),
+  };
+};
